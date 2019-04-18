b6d02015ecb1718c253042d1f25d181230fdf605
dracovoldy <dracovoldy@gmail.com>
2019-04-18 15:21:21 +0000

    18/4/2019: Nayan: Initial Development for Estimator All views are done fully Scope view is partially done.

--- webapp/controller/POCM.controller.js
+++ webapp/controller/POCM.controller.js
@@ -3,21 +3,21 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("com.limscloud.app.controller.LandingPage", {
+	return Controller.extend("com.limscloud.app.controller.POCM", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.limscloud.app.view.LandingPage
+		 * @memberOf com.limscloud.app.view.POCM
 		 */
 		onInit: function () {
-
+ this.router = this.getOwnerComponent().getRouter();
 		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.limscloud.app.view.LandingPage
+		 * @memberOf com.limscloud.app.view.POCM
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -26,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.limscloud.app.view.LandingPage
+		 * @memberOf com.limscloud.app.view.POCM
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -34,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.limscloud.app.view.LandingPage
+		 * @memberOf com.limscloud.app.view.POCM
 		 */
 		//	onExit: function() {
 		//
