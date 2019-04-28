a9963898c81072fdbea18539a2c2f1f16e047f79
dracovoldy <dracovoldy@gmail.com>
2019-04-28 15:11:00 +0000

    dev rem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path'), express = require('express');
 var favicon = require('serve-favicon');
 var qs = require('querystring');
 var app = express();
-app.use('/dev', express.static(path.join(__dirname, 'webapp')));
+app.use('/', express.static(path.join(__dirname, 'webapp')));
 app.use('/prod', express.static(path.join(__dirname, 'dist')));
 app.use(favicon(__dirname + '/webapp/favicon.ico'));
 
